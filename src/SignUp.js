989256afbdc0b91b467d0f8a6cd4c433ee3dd5f6
Lívia Brasil <liviabrasil@id.uff.br>
2021-05-17 23:58:08 -0300

    sign up post

--- src/SignUp.js
+++ src/SignUp.js
@@ -1,16 +1,37 @@
 import styled from 'styled-components';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import axios from 'axios';
 
 export default function SignUp () {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [name, setName] = useState("");
+    const [image, setImage] = useState("");
+
+    const data = {
+        email: email,
+        name: name,
+        image: image,
+        password: password
+    }
+
+    function sendData () {
+        const request = axios.post("https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/auth/sign-up", data);
+
+        request.then(console.log(request))
+    }
+
     return (
         <Page>
             <img src="img/logo.png" />
             <Form>
-                <input type="text" placeholder="email" />
-                <input type="password" placeholder="senha" />
-                <input type="text" placeholder="nome" />
-                <input type="text" placeholder="foto" />
-                <button>Cadastrar</button>
+                <input onChange={(event) => setEmail(event.target.value)} type="text" placeholder="email" />
+                <input onChange={(event) => setPassword(event.target.value)} type="password" placeholder="senha" />
+                <input onChange={(event) => setName(event.target.value)} type="text" placeholder="nome" />
+                <input onChange={(event) => setImage(event.target.value)} type="text" placeholder="foto" />
+                <button onClick={sendData}>Cadastrar</button>
             </Form>
 
             <Link to="/" exact>
@@ -45,13 +66,14 @@ const Form = styled.div `
         width:100%;
         height: 45px;
         border: 1px solid #D5D5D5;
-        color: #DBDBDB;
         border-radius: 5px;
         margin-bottom: 6px;
         padding-left: 11px;
         font-size: 20px;
     }
-
+    ::placeholder {
+        color: #DBDBDB;
+    }
     button {
         width:100%;
         height: 45px;
